0282cbe1dac9512cc33bdfffe31e7c3f00d25bd5
haitao.wang <haitao.wang@easydatalink.com>
2022-07-18 16:26:17 +0800

    v1

---
+++ src/com/yh/cn/User.java
@@ -0,0 +1,14 @@
+package com.yh.cn;
+
+/**
+ * @Author wht
+ * @Description
+ * @Date 2022/7/18-16:19
+ * @Param
+ * @return
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("hello user v1");
+    }
+}
