cc78d73acaefeaca7abc8b82ea47bc1d797f53fe
haitao.wang <haitao.wang@easydatalink.com>
2022-07-18 16:41:54 +0800

    hot fix 2

--- src/com/yh/cn/User.java
+++ src/com/yh/cn/User.java
@@ -10,13 +10,11 @@ package com.yh.cn;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello user v1");
-        System.out.println("hello user v2");
-        System.out.println("hello world v3");
-        System.out.println("hello world v4");
     }
 
     public static  void test1(){
         System.out.println("add test1 method");
         System.out.println("hot fix");
+        System.out.println("hot fix 2");
     }
 }
